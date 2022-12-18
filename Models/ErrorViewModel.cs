037c4a42960bf664f2f5b32ecf2a73eba28e4258
john <113229040+guptanikx@users.noreply.github.com>
2022-12-18 13:03:54 +0530

    add webapp

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace temp_webapp_git_deploy.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
